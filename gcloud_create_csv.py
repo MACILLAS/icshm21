86a8c3c55a0f41187a148c00f82079527acf1e3d
Juan Park <park.juan@gmail.com>
2021-10-05 11:55:02 -0400

    Start gcloud compatibility work.

---
+++ gcloud_create_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import os
+import glob
+
+"""
+Create Compatible CSV for GGloud AutoML
+https://cloud.google.com/vertex-ai/docs/datasets/prepare-image?_ga=2.14316789.-399339941.1624223008#csv
+
+"""
+
+if __name__ == "__main__":
+    raise NotImplemented
